5f71507061f48ac4de2ddbe581404955015551e4
Christy Babu <168579585+Chriz-t@users.noreply.github.com>
2025-10-08 08:35:51 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+class Multiplication{
+    public static void main(String args[]){
+        int i,num=5;
+        for(i=1;i<=10;i++){
+            System.out.println(i+"x"+num+"="+i*num);
+        }
+    }
+}
